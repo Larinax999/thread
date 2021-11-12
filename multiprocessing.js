f6ea4f6546896a3ed28d5cf73bb6f81498f4dc20
Larina <49904492+Larinax999@users.noreply.github.com>
2021-11-12 09:27:14 +0700

    more //

--- multiprocessing.js
+++ multiprocessing.js
@@ -4,6 +4,7 @@ const exec = (...args) => {args.splice(1, 0, 0);setTimeout(...args);};
 // sleep
 const sleep = (ms) => return new Promise(resolve => setTimeout(resolve, ms));
 
+// async function
 async function main(data) {
   await sleep(1500); // sleep 1.5sec
   console.log(`Hello ${data}`);
