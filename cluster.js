416d01d20ecf4547641908fd89968725dec77b33
Larina <49904492+Larinax999@users.noreply.github.com>
2021-11-12 09:55:05 +0700

    RECOMMENDED (maybe?)

--- cluster.js
+++ cluster.js
@@ -1,20 +1,20 @@
 const cluster = require('cluster');
 
 function hi() {
-  console.log("hi");
+    console.log("hi");
 }
 
 if (cluster.isMaster) {
-	let i = 5;
-	while (i--) {
-		cluster.fork().send({
-			"somemsg": "Hello World" // only string and integer
-		});
-	}
-	hi();
+    let i = 5;
+    while (i--) {
+        cluster.fork().send({
+            "somemsg": "Hello World" // only string and integer
+        });
+    }
+    hi();
 } else {
-	process.on('message', function(message) {
-		console.log(message.somemsg);
-		hi();
-	});
+    process.on('message', function (message) {
+        console.log(message.somemsg);
+        hi();
+    });
 }
