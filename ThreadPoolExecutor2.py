81b02c477fb938229b39e6bed950c7b4d3e21dee
Larina <49904492+Larinax999@users.noreply.github.com>
2021-11-12 09:53:19 +0700

    NOT RECOMMENDED

--- ThreadPoolExecutor2.py
+++ ThreadPoolExecutor2.py
@@ -2,5 +2,6 @@ from os import system
 from concurrent.futures import ThreadPoolExecutor
 executor = ThreadPoolExecutor(max_workers=int(100000))
 
+
 for i in range(10):
     executor.submit(system,'python test.py')
