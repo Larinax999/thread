db147fa7a38e6e520eb065418a02dcae1327c13e
Larina <49904492+Larinax999@users.noreply.github.com>
2021-11-12 09:55:58 +0700

    RECOMMENDED (wat de fuq int() )

--- ThreadPoolExecutor.py
+++ ThreadPoolExecutor.py
@@ -1,6 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor
 
-executor = ThreadPoolExecutor(max_workers=int(100000))
+executor = ThreadPoolExecutor(max_workers=100000)
 
 def test():
     print("hello")
